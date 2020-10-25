0331abd56ab04fbaf7c9a9f23c2ee265deefa501
samurai <justxikka@gmail.com>
2020-10-25 13:13:09 +0300

    some fixes and comments.

--- strings.py
+++ strings.py
@@ -1,47 +1,9 @@
-#### Функция выбора задания
-def taskChoice():
-    str = ""
-    try:
-        taskNumber = int(input("Введите номер задания: "))
-        if 1 > taskNumber > 30:
-            print("Такого задания нет. Попробуйте вновь")
-            taskChoice()
-        else:
-            if taskNumber == 1: taskOne()
-            if taskNumber == 2: taskTwo()
-            if taskNumber == 3: taskThree()
-            if taskNumber == 4: taskFour()
-            if taskNumber == 5: taskFive()
-            if taskNumber == 6: taskSix()
-            if taskNumber == 7: taskSeven()
-            if taskNumber == 8: taskEight()
-            if taskNumber == 9: taskNine()
-            if taskNumber == 10: taskTen()
-            if taskNumber == 11: taskEleven()
-            if taskNumber == 12: taskTwelve()
-            if taskNumber == 13: taskThirteen()
-            if taskNumber == 14: taskFourteen()
-            if taskNumber == 15: taskFifteen()
-            if taskNumber == 16: taskSixteen()
-            if taskNumber == 17: taskSeventeen()
-            if taskNumber == 18: taskEighteen()
-            if taskNumber == 19: taskNineteen()
-            if taskNumber == 20: taskTwenty()
-            if taskNumber == 21: taskTwentyOne()
-            if taskNumber == 22: taskTwentyTwo()
-            if taskNumber == 23: taskTwentyThree()
-            if taskNumber == 24: taskTwentyFour()
-            if taskNumber == 25: taskTwentyFive()
-            if taskNumber == 26: taskTwentySix()
-            if taskNumber == 27: taskTwentySeven()
-            if taskNumber == 28: taskTwentyEight()
-            if taskNumber == 29: taskTwentyNine()
-            if taskNumber == 30: taskThirty()
-    except ValueError:
-        print("Введите число.")
-
 #### Задание 7.1
-def taskOne():
+def task_1():
+
+    #Дана строка. В каждой новой строке выведите её
+    #подстроки, согласно заданию:
+
     str = "Abrakadabra"
     print("\nСтрока: ", str)
     print("Третий символ строки: ", str[2])
@@ -54,46 +16,102 @@ def taskOne():
     print("Символы через один, в обратном порядке: ",str[::-2])
     print("Длинна строки: ", len(str), "\n")
     taskChoice()
+
 #### Задание 7.2
-def taskTwo():
+def task_2():
+
+    #Напишите программу, которая отсекает всю часть строки
+    #после первого слова.
+
     str = "Once upon a time"
     print("\nСтрока: ", str)
     newStr = str.split()
     print("Первое слово: ", newStr[0], "\n")
     taskChoice()
+
 #### Задание 7.3
-def taskThree():
+def task_3():
+
+    #Дана строка, состоящая из слов, разделенных пробелами.
+    #Определите, сколько в ней слов. Используйте для решения
+    #задачи метод count. Выведите длину данной строки.
+
     str = "In the hole in the ground there lived a hobbit"
     print("\nСтрока: ", str)
     strCount = str.count(' ') + 1
     print("Всего слов: ", strCount, "\n")
     taskChoice()
+
 #### Задание 7.4
-def taskFour():
+def task_4():
+
+    #Ввести с клавиатуры символьную строку и определить,
+    #сколько в ней слов. Словом считается последовательность
+    #непробельных символов, отделенная с двух сторон
+    #пробелами (или стоящая с краю строки). Слова могут быть
+    #разделены несколькими пробелами, в начале и в конце
+    #строки тоже могут быть пробелы.
+
     str = input("\nВведите символьную строку: ").split()
     print("Всего слов: ", len(str), "\n")
     taskChoice()
+
 #### Задание 7.5
-def taskFive():
+def task_5():
+
+    #Ввести с клавиатуры символьную строку и найти самое
+    #длинное слово и его длину. Словом считается
+    #последовательности непробельных символов, отделенная с
+    #двух сторон пробелами (или стоящая с краю строки). Слова
+    #могут быть разделены несколькими пробелами, в начале и
+    #в конце строки тоже могут быть пробелы.
+
     str = input("\nВведите символьную строку: ").split()
     print(max(str, key=len))
     taskChoice()
+
 #### Задание 7.6
-def taskSix():
+def task_6():
+
+    #Дана строка, состоящая ровно из двух слов, разделенных
+    #пробелом. Переставьте эти слова местами. Результат
+    #запишите в строку и выведите получившуюся строку.
+        #При решении этой задачи не стоит пользоваться
+        #циклами и инструкцией if
+
     str = "Hello, world!"
     print("\nСтрока: ", str)
     firstStr = str[:str.find(" ")]
     secondStr = str[str.find(" "):]
     print("Переставленные слова: ", secondStr, firstStr, "\n")
     taskChoice()
+
 #### Задание 7.7
-def taskSeven():
+def task_7():
+
+    #Дана строка. Разрежьте ее на две равные части (если длина
+    #строки — четная, а если длина строки нечетная, то длина
+    #первой части должна быть на один символ больше).
+    #Переставьте эти две части местами, результат запишите в
+    #новую строку и выведите на экран.
+        #При решении этой задачи не стоит пользоваться
+        #инструкцией if
+
     str = "Hello"
     print("\nСтрока: ", str)
     print(str[(len(str) + 1) // 2:] + str[:(len(str) + 1) // 2], "\n")
     taskChoice()
+
 #### Задание 7.8
-def taskEight():
+def task_8():
+
+    #Дана строка. Если в этой строке буква f встречается только
+    #один раз, выведите её индекс. Если она встречается два и
+    #более раз, выведите индекс её первого и последнего
+    #появления. Если буква f в данной строке не встречается,
+    #ничего не выводите.
+        #При решении этой задачи не стоит использовать циклы.
+
     str = input("\nВведите строку: ").lower()
     firstEntry = str.find('f')
     lastEntry = str.rfind('f')
@@ -104,8 +122,15 @@ def taskEight():
     else:
         print(firstEntry, lastEntry, "\n")
     taskChoice()
+
 #### Задание 7.9
-def taskNine():
+def task_9():
+
+    #Дана строка. Найдите в этой строке второе вхождение
+    #буквы f, и выведите индекс этого вхождения. Если буква f в
+    #данной строке встречается только один раз, выведите
+    #число -1, а если не встречается ни разу, выведите число -2.
+
     str = input("\nВведите строку: ").lower()
     if str.count("f") == -1:
         print("-1", "\n")
@@ -114,72 +139,137 @@ def taskNine():
     else:
         print(str.find('f', str.find('f') + 1), "\n")
     taskChoice()
+
 #### Задание 7.10
-def taskTen():
+def task_10():
+
+    #Дана строка, в которой буква h встречается минимум два
+    #раза. Удалите из этой строки первое и последнее
+    #вхождение буквы h, а также все символы, находящиеся между ними.
+
     str = input("\nВведите строку: ").lower()
     str = str[:str.find('h')] + str[str.rfind('h') + 1:]
     print("Очищенная строка: ", str, "\n")
     taskChoice()
+
 #### Задание 7.11
-def taskEleven():
-    print("Go out from my swamp!")
-#### Задание 7.12
-def taskTwelve():
-    print("Go out from my swamp!")
-#### Задание 7.13
-def taskThirteen():
-    print("Go out from my swamp!")
-#### Задание 7.14
-def taskFourteen():
-    print("Go out from my swamp!")
-#### Задание 7.15
-def taskFifteen():
-    print("Go out from my swamp!")
-#### Задание 7.16
-def taskSixteen():
-    print("Go out from my swamp!")
-#### Задание 7.17
-def taskSeventeen():
-    print("Go out from my swamp!")
-#### Задание 7.18
-def taskEighteen():
-    print("Go out from my swamp!")
-#### Задание 7.19
-def taskNineteen():
-    print("Go out from my swamp!")
-#### Задание 7.20
-def taskTwenty():
-    print("Go out from my swamp!")
-#### Задание 7.21
-def taskTwentyOne():
-    print("Go out from my swamp!")
-#### Задание 7.22
-def taskTwentyTwo():
-    print("Go out from my swamp!")
-#### Задание 7.23
-def taskTwentyThree():
-    print("Go out from my swamp!")
-#### Задание 7.24
-def taskTwentyFour():
-    print("Go out from my swamp!")
-#### Задание 7.25
-def taskTwentyFive():
-    print("Go out from my swamp!")
-#### Задание 7.26
-def taskTwentySix():
-    print("Go out from my swamp!")
-#### Задание 7.27
-def taskTwentySeven():
-    print("Go out from my swamp!")
-#### Задание 7.28
-def taskTwentyEight():
-    print("Go out from my swamp!")
-#### Задание 7.29
-def taskTwentyNine():
-    print("Go out from my swamp!")
-#### Задание 7.30
-def taskThirty():
+def task_11():
     print("Go out from my swamp!")
 
-#### Вызов стартовой функции выбора задания
+#### Задание 7.12
+def task_12():
+    print("Go out from my swamp!")
+
+#### Задание 7.13
+def task_13():
+    print("Go out from my swamp!")
+
+#### Задание 7.14
+def task_14():
+    print("Go out from my swamp!")
+
+#### Задание 7.15
+def task_15():
+    print("Go out from my swamp!")
+
+#### Задание 7.16
+def task_16():
+    print("Go out from my swamp!")
+
+#### Задание 7.17
+def task_17():
+    print("Go out from my swamp!")
+
+#### Задание 7.18
+def task_18():
+    print("Go out from my swamp!")
+
+#### Задание 7.19
+def task_19():
+    print("Go out from my swamp!")
+
+#### Задание 7.20
+def task_20():
+    print("Go out from my swamp!")
+
+#### Задание 7.21
+def task_21():
+    print("Go out from my swamp!")
+
+#### Задание 7.22
+def task_22():
+    print("Go out from my swamp!")
+
+#### Задание 7.23
+def task_23():
+    print("Go out from my swamp!")
+
+#### Задание 7.24
+def task_24():
+    print("Go out from my swamp!")
+
+#### Задание 7.25
+def task_25():
+    print("Go out from my swamp!")
+
+#### Задание 7.26
+def task_26():
+    print("Go out from my swamp!")
+
+#### Задание 7.27
+def task_27():
+    print("Go out from my swamp!")
+
+#### Задание 7.28
+def task_28():
+    print("Go out from my swamp!")
+
+#### Задание 7.29
+def task_29():
+    print("Go out from my swamp!")
+
+#### Задание 7.30
+def task_30():
+    print("Go out from my swamp!")
+
+#### Функция выбора задания
+def taskChoice():
+    str = ""
+    try:
+        taskNumber = int(input("Введите номер задания: "))
+        if taskNumber == 1: task_1()
+        elif taskNumber == 2: task_2()
+        elif taskNumber == 3: task_3()
+        elif taskNumber == 4: task_4()
+        elif taskNumber == 5: task_5()
+        elif taskNumber == 6: task_6()
+        elif taskNumber == 7: task_7()
+        elif taskNumber == 8: task_8()
+        elif taskNumber == 9: task_9()
+        elif taskNumber == 10: task_10()
+        elif taskNumber == 11: task_11()
+        elif taskNumber == 12: task_12()
+        elif taskNumber == 13: task_13()
+        elif taskNumber == 14: task_14()
+        elif taskNumber == 15: task_15()
+        elif taskNumber == 16: task_16()
+        elif taskNumber == 17: task_17()
+        elif taskNumber == 18: task_18()
+        elif taskNumber == 19: task_19()
+        elif taskNumber == 20: task_20()
+        elif taskNumber == 21: task_21()
+        elif taskNumber == 22: task_22()
+        elif taskNumber == 23: task_23()
+        elif taskNumber == 24: task_24()
+        elif taskNumber == 25: task_25()
+        elif taskNumber == 26: task_26()
+        elif taskNumber == 27: task_27()
+        elif taskNumber == 28: task_28()
+        elif taskNumber == 29: task_29()
+        elif taskNumber == 30: task_30()
+        else:
+            print("Такого задания нет. Попробуйте вновь")
+            taskChoice()
+    except ValueError:
+        print("Введите число.")
 taskChoice()
